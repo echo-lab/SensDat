0fe533a6d7a3b43fbdb2776534938de13d0f0b9d
andyluu1-admin <andyluu324@gmail.com>
2023-06-20 18:03:15 -0400

    Deleted Icons.js and converted icons to react icons

--- src/viz-view.js
+++ src/viz-view.js
@@ -22,7 +22,7 @@ import { hhmmss } from "./utils.js";
 import { UIState } from "./ui-state.js";
 import { DataEditor } from "./viz-data-editor.js";
 import { UploadLayoutWidget } from "./upload-layout.js";
-import { SquareIcon, CircleIcon, GearIcon } from "./icons.js";
+import { BsSquare,BsCircle,BsGearFill  } from "react-icons/bs";
 
 import { PXL_HEIGHT, PXL_WIDTH } from "./constants.js";
 
@@ -290,7 +290,7 @@ function SettingsWidgets({
           id="dropdown-basic"
           disabled={uiState.busy()}
         >
-          <GearIcon />
+          <BsGearFill />
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
@@ -527,7 +527,7 @@ function RegionShapeSelector({ svgWidth, createRegionInteraction }) {
           createRegionInteraction.useEllipse();
         }}
       >
-        <CircleIcon />
+        <BsCircle />
       </ListGroup.Item>
       <ListGroup.Item
         variant="light"
@@ -538,7 +538,7 @@ function RegionShapeSelector({ svgWidth, createRegionInteraction }) {
           createRegionInteraction.useRect();
         }}
       >
-        <SquareIcon />
+        <BsSquare />
       </ListGroup.Item>
     </ListGroup>
   );
