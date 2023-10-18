ab4b74fd201cd95c6f3f71215fa257c29e3c53d9
Andy Luu <andyluu324@gmail.com>
2023-10-18 17:04:28 -0400

    split homepage

--- src/home-page.js
+++ src/home-page.js
@@ -5,24 +5,28 @@ import "./home-page.css";
 const Home = () => {
   return (
     <div className="Home">
-      <h1 className="Home-title">Welcome to Octave!</h1>
-      <div className="Home-description">
-        Octave is a website aimed at allowing you to create Observable
-        Connections between Tables Algorithms and Visualizations! Here is a
-        Tutorial Video on how to use the site!
+      <div className="Home-left">
+        <h1 className="Home-title">Welcome to Octave!</h1>
+        <div className="Home-description">
+          Octave is a website aimed at allowing you to create Observable
+          Connections between Tables Algorithms and Visualizations! Here is a
+          Tutorial Video on how to use the site!
+        </div>
+        <iframe
+          width="900"
+          height="600"
+          src=""
+          title="Octave Tutorial"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
       </div>
-      <iframe
-        width="900"
-        height="600"
-        src=""
-        title="Octave Tutorial"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
-      <a className="Home-spatialTemporalButton" href="spatial-temporal">
-        Check Out Octave Here!
-      </a>
+      <div className="Home-right">
+        <a className="Home-spatialTemporalButton" href="spatial-temporal">
+          Check Out Octave Here!
+        </a>
+      </div>
     </div>
   );
 };
